fd974c721ab4462bff83e302bc91017a09c71b66
kmiller <kmmiller418@gmail.com>
2022-05-02 16:04:16 -0400

    Add printing problems

---
+++ HiFriend/src/HiFriend.java
@@ -0,0 +1,7 @@
+public class HiFriend {
+    public static void main(String[] args) throws Exception {
+        String greeting = "Hi";
+        String friend = "friendo-wiendo";
+        System.out.println(greeting + " " + friend);
+    }
+}
